dc6b3463c4908c57ebb014881ccbeedeadb70ecc
jodosjodos <jeandedieu2030@gmail.com>
2023-07-16 18:40:45 +0200

    make cleaner codes

--- index.js
+++ index.js
@@ -7,30 +7,34 @@ const getComputerChoice = () => {
 
 const playRound = (personOption, computerOption) => {
   let result = "";
+  console.log(personOption);
+  if (personOption && computerOption) {
+    const personOptionLower = personOption.toLowerCase();
+    const computerOptionLower = computerOption.toLowerCase();
 
-  const personOptionLower = personOption.toLowerCase();
-  const computerOptionLower = computerOption.toLowerCase();
-
-  if (personOptionLower === computerOptionLower) {
-    result = "Draw happened";
-  } else if (personOptionLower === "rock") {
-    if (computerOptionLower === "scissor") {
-      result = "You have won! Rock beats scissor";
-    } else {
-      result = "You have lost! Paper beats rock";
+    if (personOptionLower === computerOptionLower) {
+      result = "Draw happened";
+    } else if (personOptionLower === "rock") {
+      if (computerOptionLower === "scissor") {
+        result = "You have won! Rock beats scissor";
+      } else {
+        result = "You have lost! Paper beats rock";
+      }
+    } else if (personOptionLower === "scissor") {
+      if (computerOptionLower === "paper") {
+        result = "You have won! Scissor beats paper";
+      } else {
+        result = "You have lost! Rock beats scissor";
+      }
+    } else if (personOptionLower === "paper") {
+      if (computerOptionLower === "rock") {
+        result = "You have won! Paper beats rock";
+      } else {
+        result = "You have lost! Scissor beats paper";
+      }
     }
-  } else if (personOptionLower === "scissor") {
-    if (computerOptionLower === "paper") {
-      result = "You have won! Scissor beats paper";
-    } else {
-      result = "You have lost! Rock beats scissor";
-    }
-  } else if (personOptionLower === "paper") {
-    if (computerOptionLower === "rock") {
-      result = "You have won! Paper beats rock";
-    } else {
-      result = "You have lost! Scissor beats paper";
-    }
+  } else {
+    alert("Please provide values");
   }
 
   return result;
